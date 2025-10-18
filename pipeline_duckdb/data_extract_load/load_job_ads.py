4f3502eda0b2c9dbc918071668f13d61d99e7cee
abdulrahmanfahmi <abdirahman3016@gmail.com>
2025-10-18 17:19:09 +0200

    definitions

--- pipeline_duckdb/data_extract_load/load_job_ads.py
+++ pipeline_duckdb/data_extract_load/load_job_ads.py
@@ -8,7 +8,7 @@ import json
 # truncate staging_staging schema produced by dlt together with dagster by default
 dlt.config["load.truncate_staging_dataset"] = True
 
-params = {"limit": 100, "occupation-field": "6Hq3_tKo_V57"}
+params = { "limit": 100, "occupation-field": "6Hq3_tKo_V57"}
 
 def _get_ads(url_for_search, params):
     response = requests.get(url_for_search, params=params)
